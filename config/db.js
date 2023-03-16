921790429c9f6003f5fee94b677b513681621f50
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-03-16 15:26:31 +0530

    db conn success

---
+++ config/db.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+var db = null;
+class database {
+  constructor() {
+    const { MONGO_URI } = process.env;
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+    if (db) {
+      return db;
+    }
+    this._connect();
+    return db;
+  }
+
+  _connect() {
+    mongoose.set("strictQuery", true);
+    mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Database connection successful");
+      })
+      .catch((err) => {
+        console.error("Database connection error");
+      });
+
+    db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+    db.once("open", function () {
+      console.log("We're connected!");
+    });
+
+    db.on("disconnected", function () {
+      console.log("Mongoose default connection is disconnected");
+    });
+
+  }
+}
+
+module.exports = new database();
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 require("dotenv").config();
- express = require("express");
-
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello JPMC!");
+
+const Database = require("./config/db");
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(express.json({ limit: "5mb" })); // middleware to use req.body
+app.use(cors()); // to avoid CORS errors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.get("/", (req, res) => {
+  res.json({ status: `OK at ${new Date()}` });
+});
+
+// middleware to act as fallback for all 404 errors
+// app.use(notFound);
+
+// // configure a custome error handler middleware
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
+                
         console.log(`http://localhost:${PORT}`);
     }
 );
